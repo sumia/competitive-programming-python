849a2df6ef3b53e4ee860f5633562c68f16b6d7d
Sumia Akter <sumia.017@gmail.com>
2024-07-09 17:45:13 +0100

    print output to show where fast and slow pointers meet

--- src/two-pointers-detect-cycle.py
+++ src/two-pointers-detect-cycle.py
@@ -15,9 +15,11 @@ def has_cycle(head):
     slow, fast = head, head
 
     while fast and fast.next:
+        print(f"slow: {slow.val}, fast: {fast.val}")
         slow = slow.next
         fast = fast.next.next
         if slow == fast:
+            print(f"slow: {slow.val}, fast: {fast.val}")
             return True
     
     return False
@@ -27,6 +29,8 @@ node1 = ListNode(1)
 node2 = ListNode(2)
 node3 = ListNode(3)
 node4 = ListNode(4)
+node5 = ListNode(5)
+node6 = ListNode(6)
 
 # Link nodes to without cycle: 1 -> 2 -> 3 -> 4 
 node1.next = node2
@@ -44,3 +48,13 @@ node2.next = node3
 node3.next = node4
 node4.next = node2
 print(has_cycle(node1)) # True
+
+
+# Link nodes to form cycle: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 2
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node2
+print(has_cycle(node1))
